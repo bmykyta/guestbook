bad93b123aa103040cbea4385d87fa63f4160ea5
Mykyta <bondarchuk.nik.nb@gmail.com>
2023-03-28 22:04:15 +0300

    Create comment form type and show it on the page

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,8 +23,7 @@ class ConferenceController extends AbstractController
     public function index(ConferenceRepository $conferenceRepository): Response
     {
         return new Response(
-            $this->twig->render('conference/index.html.twig', [
-            ])
+            $this->twig->render('conference/index.html.twig', [])
         );
     }
 
@@ -32,15 +33,18 @@ class ConferenceController extends AbstractController
         Conference        $conference,
         CommentRepository $commentRepository
     ): Response {
+        $comment   = new Comment();
+        $form      = $this->createForm(CommentFormType::class, $comment);
         $offset    = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
         return new Response(
             $this->twig->render('conference/show.html.twig', [
-                'conference' => $conference,
-                'comments'   => $paginator,
-                'previous'   => $offset - CommentRepository::PAGINATOR_PER_PAGE,
-                'next'       => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+                'conference'   => $conference,
+                'comments'     => $paginator,
+                'previous'     => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next'         => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+                'comment_form' => $form->createView(),
             ])
         );
     }
