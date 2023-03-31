f5631e9253f92428a2fb9bb942d48de23d9b283f
Mykyta <bondarchuk.nik.nb@gmail.com>
2023-03-31 23:32:09 +0300

    Write new fixtures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use App\Entity\Comment;
+use App\Entity\Conference;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class AppFixtures extends Fixture
+{
+    public function __construct(
+        private readonly PasswordHasherFactoryInterface $passwordHasherFactory,
+        private readonly SluggerInterface $slugger
+    )
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $amsterdam = new Conference();
+        $amsterdam
+            ->setCity('Amsterdam')
+            ->setYear('2019')
+            ->setIsInternational(true)
+            ->computeSlug($this->slugger);
+        $manager->persist($amsterdam);
+
+        $paris = new Conference();
+        $paris->setCity('Paris')->setYear('2020')->setIsInternational(false);
+        $manager->persist($paris);
+
+        $comment1 = new Comment();
+        $comment1
+            ->setConference($amsterdam)
+            ->setAuthor('Mykyta')
+            ->setEmail('mykyta@example.com')
+            ->setText('This was a great conference.')
+        ;
+        $manager->persist($comment1);
+
+        $admin = new Admin();
+        $admin
+            ->setRoles(['ROLE_ADMIN'])
+            ->setUsername('admin')
+            ->setPassword($this->passwordHasherFactory->getPasswordHasher(Admin::class)->hash('admin'));
+        $manager->persist($admin);
+
+        $manager->flush();
+    }
+}
